5ba59e9074c3c79d9a4247182608c8210db257af
Raz Buchnik <raz@taxi4you.co.il>
2020-05-21 10:27:36 +0300

    tiny

--- src/routes/Page.jsx
+++ src/routes/Page.jsx
@@ -11,7 +11,7 @@ const Handler = ({_id}) => {
         Page with props, user.type: { user.type }
       </Block>
       <Block>
-        <Button fill color="blue" onClick={() => user.SetType(_id)}>
+        <Button fill color="blue" onClick={() => user.SetType(+ new Date())}>
           Update user type
         </Button>
       </Block>
