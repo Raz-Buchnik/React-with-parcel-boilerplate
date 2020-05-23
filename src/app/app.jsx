8cac70ba67ad8943e12249d0da73f176b604dca5
Raz Buchnik <raz@taxi4you.co.il>
2020-05-23 20:29:27 +0300

    first component is functional instead of class

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,29 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { App, View, f7ready } from 'framework7-react'
 import AppParams from '/app/params'
 import AppRouter from '/app/router'
 import { view as State } from 'react-easy-state'
 import { theme, language, global } from '/stores'
 
-class AppMain extends React.Component {
-  constructor() {
-    super()
-  }
-  componentDidMount() {
-    f7ready(f7App => {
-      global.app = f7App
-      global.app.dialog.alert('Hello World')
-    })
-  }
-  render() {
-    return (
-      <App params={AppParams(language)} routes={AppRouter} className={`${theme.current}`}>
-        { 
-          global.app && <View main pushState url="/" />
-        }
-      </App>
-    )
-  }
+const InitF7Effect = () => {
+  f7ready(f7App => {
+    global.app = f7App
+    global.app.dialog.alert("Hello World")
+  })
+}
+
+const AppMain = () => {
+  useEffect(InitF7Effect, [])
+
+  return (
+    <App params={AppParams(language)} routes={AppRouter} className={`${theme.current}`}>
+      { 
+        global.app && <View main pushState url="/" />
+      }
+    </App>
+  )
 }
 
 export default State(AppMain)
